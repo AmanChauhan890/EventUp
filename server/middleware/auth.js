e36d5c503ed8806b1b872cf9d97c04de2f69ce3a
Aman <codingaman1901@gmail.com>
2026-05-12 22:55:48 +0530

    added a base case

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,4 +1,4 @@
-const jwt = require('jasonwebtoken');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User.js');
 
 const protect = async (req, res, next) => {
@@ -8,6 +8,9 @@ const protect = async (req, res, next) => {
         try{
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
             req.user = await User.findById(decoded.id).select('-password');
+             if (!req.user) {
+                return res.status(401).json({ message: 'Not authorized, user not found' });
+            }
             next();
         }catch(err){
             res.status(401).json({ message: 'Not authorized, token failed' });
@@ -18,7 +21,7 @@ const protect = async (req, res, next) => {
 };
 
 const admin = (req, res, next) => {
-    if(req.user && req.user.isAdmin){
+    if(req.user && req.user.role === 'admin'){
         next();
     }else{
         res.status(403).json({ message: 'Not authorized as admin' });
